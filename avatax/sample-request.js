72d027ead8d848c7f0b3b18bc33086cb4c5bc9b0
Ben Johnson <ben.johnson@avalara.com>
2016-06-16 14:02:51 -0700

    Fill sample data functionality

--- avatax/sample-request.js
+++ avatax/sample-request.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// just routes data to the view
-var dumbController = function(data) {
-    return data;
-};
-
 // renders sample request metadata
 var renderMetadata = function(metadata) {
     return metadata.map(function(md) {
@@ -27,33 +22,50 @@ var renderQuerystring = function(querystring) {
     ]);
 };
 
-var renderPostBody = function(postBody) {
-    if (!Object.keys(postBody).length) {
+var renderPostBody = function(showPostBody, postBody, samplePostBody) {
+    if (!showPostBody) {
         return;
     }
 
-    return m('textarea', {rows: "10", cols: "50"}, JSON.stringify(postBody, null, 2));
-}
+    return [
+        m('textarea', {rows: "10", cols: "50"}, JSON.stringify(postBody(), null, 2)),
+        m('br'),
+        m('button', {onclick: function(e) {postBody(samplePostBody)}}, 'Fill sample data')
+    ];
+};
+
+var controller = function(data) {
+        var vm = {
+            title: data.title,
+            metadata: data.metadata,
+            querystring: data.querystring,
+            showPostBody: data.postBody ? true : false,
+            samplePostBody: data.postBody,
+            postBody: m.prop({})
+        };
+
+        return vm;
+};
+
+var view = function(vm) {
+    logState(vm);
+    return m('div', [
+        m('h3', vm.title),
+        m('table', renderMetadata(vm.metadata)),
+        renderQuerystring(vm.querystring),
+        renderPostBody(vm.showPostBody, vm.postBody, vm.samplePostBody),
+    ]);
+};
+
+var viewModel = {};
 
 // view for a Sample API Request
 var SampleReq = {
-    controller: dumbController,
-    view: function(ctrl) {
-        return m('div', [
-            m('h3', ctrl.title),
-            m('table', renderMetadata(ctrl.metadata)),
-            renderQuerystring(ctrl.querystring),
-            renderPostBody(ctrl.postBody)
-        ]);
-    }
+    controller: controller,
+    view: view,
+    viewModel: viewModel
 };
 
-// app wrapper
-var App = {
-    controller: dumbController,
-    view: function(data) {
-        return m('div', [
-           data.map(function(reqObj) {return m.component(SampleReq, reqObj)}) 
-        ]);
-    }
-};
+function logState(vm) {
+    console.log('Post Body', JSON.stringify(vm.postBody(), null, 2));
+}
