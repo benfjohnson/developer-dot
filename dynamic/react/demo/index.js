372f5c96aa6eaf995fc23f4b4e6dd24fde1eae43
Josh Sherman <josh.sherman@avalara.com>
2016-06-23 10:41:56 -0700

    starting on converting swagger post body

--- dynamic/react/demo/index.js
+++ dynamic/react/demo/index.js
@@ -24,6 +24,47 @@ const API_SWAGGER_URLS = {
 };
 const API_SWAGGER_URL = API_SWAGGER_URLS[API].base + API_SWAGGER_URLS[API].api;
 
+const createPostBody = (endpoint, definitions, currBody = {}) => {
+    const buildSchema = (schemaName, schema) => {
+        console.log('SCHEMA NAME', schemaName);
+        console.log('SCHEMA', schema);
+        if (schema.$ref) {
+            const refArray = schema.$ref.split('/');
+            const ref = refArray[refArray.length -1];
+            console.log(`RECURSING FOR REF ${ref}`);
+
+            const refObj = definitions[ref];
+
+            return refObj.type === 'string' ? {type: refObj.type, example: refObj.example || 'foo'}: buildSchema(ref, definitions[ref], currBody);
+        }
+
+        if (schema.type && schema.type ==='object') {
+            return Object.assign({}, ...Object.keys(schema.properties).map((name) => {
+                console.log(`RECURSING FOR OBJ PROPERTY ${name}`);
+                return {...currBody, [name]: buildSchema(name, schema.properties[name], currBody)};
+            }));
+        }
+
+        if (schema.type && schema.type === 'string') {
+            console.log('RETURNING STRING PROPERTY', schemaName);
+            return {type: schema.type, example: schema.example || 'foo'};
+        }
+    };
+
+    const params = endpoint.parameters || [];
+    let postBody = params.filter((p) => p.in === 'body');
+    if (postBody.length) {
+        postBody = postBody[0];
+        postBody = buildSchema('postBody', {
+            "$ref": "#/definitions/CalculateBase"
+        });
+        console.log('POST BODY', postBody);
+    }
+
+    return postBody
+};
+
+
 const sanitizeSwagger = (api) => {
     const routes = Object.keys(api.paths).map((path) => {
         return Object.keys(api.paths[path]).map((action) => {
@@ -45,7 +86,8 @@ const sanitizeSwagger = (api) => {
                 return queryObj;
             }, {});
 
-            // const postBodyParams = api.paths[path][action].parameters.filter((p) => p.in === 'body');
+//             const postBody = createPostBody(api.paths[path][action], api.definitions);
+
             const endpointInfo = {
                 path: routePath,
                 action: action,
@@ -56,11 +98,18 @@ const sanitizeSwagger = (api) => {
             if (Object.keys(queryString).length) {
                 endpointInfo.queryString = queryString;
             }
+
+//             if (Object.keys(postBody).length) {
+//                 endpointInfo.postBody = postBody;
+//             }
+            
             return endpointInfo;
         });
     });
 
-    return [].concat(...routes);
+    const res = [].concat(...routes);
+    res[0].postBody = createPostBody(api.paths['/v3/calculate'].post, api.definitions);
+    return res;
 };
 
 request(API_SWAGGER_URL, (error, response, body) => {
