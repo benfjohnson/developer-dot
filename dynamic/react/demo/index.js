cb3c404dc8e81ed9fa5c6a2b69c4900ac2ff349f
Ben Johnson <ben.johnson@avalara.com>
2016-06-28 17:04:07 -0700

    UBER

--- dynamic/react/demo/index.js
+++ dynamic/react/demo/index.js
@@ -4,6 +4,9 @@ import request from 'request';
 import App from './App';
 import {store} from './store';
 
+// import * as SwaggerUi from '../../dist/swagger-ui';
+import parseSwaggerUi from './parseSwaggerUI';
+
 import {actionTypes} from './reducers/reducer';
 
 store.subscribe(() => {
@@ -23,138 +26,33 @@ const API_SWAGGER_URLS = {
         api: '/v3/api-definition'
     }
 };
-const API_SWAGGER_URL = API_SWAGGER_URLS[API].base + API_SWAGGER_URLS[API].api;
 
-const createPostBody = (endpoint, definitions) => {
-    const buildSchema = (schemaName, schema) => {
-        if (schema['x-visibility'] === 'hidden') {
-            return;
-        }
+const API_SWAGGER_URL = './uber.yaml';
+// const API_SWAGGER_URL = API_SWAGGER_URLS[API].base + API_SWAGGER_URLS[API].api;
 
-        if (schema.hasOwnProperty('allOf')) {
-            return schema.allOf.map((allofItem) => {
-                return buildSchema(null, allofItem);
-            }).reduce((accum, obj) => ({...accum, ...obj}), {});
-        }
+window.swaggerUi = new SwaggerUi({
+    url: API_SWAGGER_URL,
+    dom_id: "swagger-ui-container",
+    supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
+    onComplete: function(swaggerApi, swaggerUi) {
+        const apiInfo = parseSwaggerUi(swaggerApi, API_SWAGGER_URL);
 
-        if (schema.$ref) {
-            const refArray = schema.$ref.split('/');
-            const ref = refArray[refArray.length - 1];
+        console.log('PARSED DATA', apiInfo);
 
-            return buildSchema(ref, definitions[ref]);
-        }
-
-        if (schema.type && schema.type === 'object') {
-            return Object.assign({uiState: {visible: false}}, ...Object.keys(schema.properties).map((name) => {
-                if (schema.properties[name] && schema.properties[name]['x-visibility'] && schema.properties[name]['x-visibility'] === 'hidden') {
-                    return;
-                }
-
-                return {[name]: buildSchema(name, schema.properties[name])};
-            }));
-        }
-
-        if (schema.type && schema.type === 'array') {
-            if (schema.items[schemaName] && schema.items[schemaName]['x-visibility'] && schema.items[schemaName]['x-visibility'] === 'hidden') {
-                return;
-            }
-
-            return {uiState: {visible: false}, fieldType: schema.type, items: buildSchema(schemaName, schema.items), value: []};
-        }
-
-        const objToReturn = {fieldType: schema.type, value: ''};
-
-        if (schema.example) {
-            objToReturn.example = schema.example;
-        }
-        if (schema.description) {
-            objToReturn.description = schema.description;
-        }
-        if (schema.enum) {
-            objToReturn.enum = schema.enum;
-        }
-        if (schema.format) {
-            objToReturn.format = schema.format;
-        }
-        if (schema.hasOwnProperty('minimum')) {
-            objToReturn.minimum = schema.minimum;
-        }
-        if (schema.hasOwnProperty('maximum')) {
-            objToReturn.maximum = schema.maximum;
-        }
-
-        return objToReturn;
-    };
-
-    const params = endpoint.parameters || [];
-    let postBody = params.filter((p) => p.in === 'body');
-
-    if (postBody.length) {
-        postBody = postBody[0];
-        postBody = buildSchema('postBody', postBody.schema);
-    }
-
-    return postBody;
-};
-
-const sanitizeSwagger = (api) => {
-    const routes = Object.keys(api.paths).map((path) => {
-        return Object.keys(api.paths[path]).map((action) => {
-            let routePath = API_SWAGGER_URLS[API].base + path;
-
-            if (api.schemes[0] && api.host && api.basePath) {
-                routePath = api.schemes[0] + '://' + api.host + (api.basePath !== '/' ? api.basePath : '') + path;
-            }
-
-            const params = api.paths[path][action].parameters || [];
-            const queryString = params.filter((p) => p.in === 'query').reduce((queryObj, param) => {
-                queryObj[param.name] = {
-                    description: param.description,
-                    required: param.required,
-                    value: '',
-                    example: param.example || ''
-                };
-                return queryObj;
-            }, {});
-
-            const postBody = createPostBody(api.paths[path][action], api.definitions);
-
-            const endpointInfo = {
-                path: routePath,
-                action: action,
-                name: api.paths[path][action].summary,
-                description: api.paths[path][action].description
-            };
-
-            if (Object.keys(queryString).length) {
-                endpointInfo.queryString = queryString;
-            }
-
-            if (Object.keys(postBody).length) {
-                endpointInfo.postBody = postBody;
-            }
-
-            return endpointInfo;
-        });
-    });
-
-    return [].concat(...routes);
-};
-
-request(API_SWAGGER_URL, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
         store.dispatch({
             type: actionTypes.FETCH_API_DATA_DONE,
-            apiInfo: [],
-            error: error ? error.message : response.statusMessage || ''
+            apiInfo: apiInfo
         });
-        return;
-    }
-    // todo try/catch around JSON.parse
-    const apiInfo = sanitizeSwagger(JSON.parse(body));
 
-    store.dispatch({
-        type: actionTypes.FETCH_API_DATA_DONE,
-        apiInfo: apiInfo
-    });
+        $('#swagger-ui-container').remove();
+    },
+    onFailure: function(data) {
+        log("Unable to Load SwaggerUI");
+    },
+    docExpansion: "none",
+    jsonEditor: false,
+    defaultModelRendering: 'schema',
+    showRequestHeaders: false
 });
+
+window.swaggerUi.load();
---
+++ dynamic/react/demo/parseSwaggerUI.js
@@ -0,0 +1,123 @@
+export default (api, rootPath) => {
+    // Build base URL path (e.g. http://localhost:8082/v3)
+    // var rootPath = api.scheme + '://' + api.host + api.basePath === '/' ? '' : api.basePath;
+
+    if (api.schemes[0] && api.host && api.basePath) {
+        rootPath = api.schemes[0] + '://' + api.host + (api.basePath !== '/' ? api.basePath : '');
+    }
+
+    var swagData = [];
+
+    api.apisArray.forEach(function(apiDef) {
+        // For every endpoint (api path + method) build a req 
+        apiDef.operationsArray.forEach(function(endpoint) {
+            // If we've already seen this endpoint in a different operationsArray, ignore:
+            if (swagData.some(function(ep) {
+                return ep.name === endpoint.summary;
+            })) {
+                return;
+            }
+
+            var apiMethod = {
+                name: endpoint.summary,
+                description: endpoint.description,
+                path: rootPath + endpoint.path,
+                action: endpoint.method
+            };
+
+            var endpointParams = endpoint.parameters || [];
+            var queryString = buildQueryString(endpointParams);
+            var postBody = buildPostBody(endpointParams);
+
+            if (Object.keys(queryString).length) {
+                apiMethod.queryString = queryString;
+            }
+            if (postBody) {
+                apiMethod.postBody = postBody;
+            }
+
+            swagData.push(apiMethod);
+        });
+    });
+
+    return swagData;
+};
+
+// Given array of parameters, filters out non-query string params and converts them to consummable shape
+function buildQueryString(endpointParams) {
+    return endpointParams
+        .filter(function(p) {
+            return p.in === 'query'
+        })
+        .reduce(function(queryObj, p) {
+            queryObj[p.name] = {
+                description: p.description,
+                required: p.required,
+                value: '',
+                example: p.example || ''
+            };
+            return queryObj;
+        }, {});
+}
+
+function buildPostBody(endpointParams) {
+    var postBodyParams = endpointParams.filter(function(p) {
+        return p.in === 'body';
+    });
+
+    // Can only be one post body per request, so safe to take first item
+    return postBodyParams.length ? buildSchema('postBody', postBodyParams[0].schema, postBodyParams[0].schema.definitions) : null;
+
+}
+
+function buildSchema(schemaName, schema, definitions) {
+    if (schema.hasOwnProperty('allOf')) {
+        return schema.allOf.map(function(chunk) {
+            return buildSchema(null, chunk, definitions);
+        }).reduce(function(accum, chunk) {
+            return Object.assign({}, accum, chunk);
+        }, {});
+    }
+
+    if (schema.$ref) {
+        var refArray = schema.$ref.split('/');
+        var ref = refArray[refArray.length - 1];
+
+        return buildSchema(ref, definitions[ref], definitions);
+    }
+
+    if (schema.type && schema.type === 'object') {
+        var nestedSchemaProps = Object.keys(schema.properties).map(function(propName) {
+            return { [propName]: buildSchema(propName, schema.properties[propName], definitions) };
+        });
+
+        return Object.assign({ uiState: { visible: false } }, ...nestedSchemaProps);
+    }
+
+    if (schema.type && schema.type === 'array') {
+        return { uiState: { visible: false }, fieldType: schema.type, items: buildSchema(schemaName, schema.items, definitions), value: [] };
+    }
+
+    const objToReturn = { fieldType: schema.type, value: '' };
+
+    if (schema.example) {
+        objToReturn.example = schema.example;
+    }
+    if (schema.description) {
+        objToReturn.description = schema.description;
+    }
+    if (schema.enum) {
+        objToReturn.enum = schema.enum;
+    }
+    if (schema.format) {
+        objToReturn.format = schema.format;
+    }
+    if (schema.hasOwnProperty('minimum')) {
+        objToReturn.minimum = schema.minimum;
+    }
+    if (schema.hasOwnProperty('maximum')) {
+        objToReturn.maximum = schema.maximum;
+    }
+
+    return objToReturn;
+}
