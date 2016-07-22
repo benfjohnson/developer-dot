9d430b0a50f5fd462f5d9526be63a79d20c21d76
Josh Sherman <josh.sherman@avalara.com>
2016-07-22 16:19:04 -0700

    Webfont, header start, styling from Abigail

--- js/site.js
+++ js/site.js
@@ -1,59 +1,95 @@
-function orderNav() {
-    var list,
-        section,
-        header,
-        sections = [],
-        lists = {},
-        headers = {};
+submitSearch = function() {
+    var $searchInput = $('#nav-query');
 
-    var navUl = document.querySelectorAll('#navigation ul')[0],
-        navLis = document.querySelectorAll('#navigation ul li');
+    $('.hdr-search-form').on('submit', function(e) {
+        e.preventDefault();
 
-    if (!navUl) return;
-
-    for (var i = 0; i < navLis.length; i++) {
-        var order, li = navLis[i];
-
-        if (li.classList.contains('nav-header')) {
-            section = li.textContent || li.innerText;
-            sections.push(section);
-            headers[section] = li;
-            continue;
+        $searchInput.removeClass('error');
+        if (!$searchInput.val()) {
+            $searchInput.addClass('error');
+            return false;
         }
 
-        if (!lists[section]) {
-            lists[section] = [];
+        var newurl = '/search/?q=' + encodeURIComponent($searchInput.val());
+        var product = $('#product').text();
+
+        if (product) {
+            newurl += '&product=' + encodeURIComponent(product);
         }
+        window.location.href = newurl;
+    });
+};
 
-        order = parseFloat(li.getAttribute('data-order'))
-        lists[section].push([order, li]);
-    }
+toggleSearchIconForm = function() {
+    var $searchForm = $('.hdr-search-form').hide();
+    var $searchFormIcon = $('.hdr-search-icon');
+    $searchFormIcon.on('click', function() {
+        $searchFormIcon.hide();
+        $searchForm.show();
+    });
+};
 
-    for (var i = 0; i < sections.length; i++) {
-        section = sections[i];
-        list = lists[section].sort(function(a, b) {
-            return a[0] - b[0];
-        });
-
-        if (header = headers[section]) {
-            navUl.appendChild(header);
-        }
-        for (var j = 0; j < list.length; j++) {
-            navUl.appendChild(list[j][1]);
-        }
-    }
-}
-
-if (document.querySelectorAll) {
-    orderNav();
-}
+getParameterByName = function(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
 
 $(document).foundation();
-
 $(document).ready(function() {
-    $('.nav-search-button').click( function(e) {
-        var newurl = '/search/?q=' + encodeURIComponent($("#nav-query").val());
-        if ($("#product").text()) newurl += "&product="+ encodeURIComponent($("#product").text());
+    submitSearch();
+    toggleSearchIconForm();
+
+    var queryparam = getParameterByName('q');
+    $("#query").val(queryparam);
+
+    var productfacet = getParameterByName('product');
+    var doctypefacet = getParameterByName('doctype');
+
+    $("#product-facet").val(productfacet);
+    $("#doctype-facet").val(doctypefacet);
+
+    var client = algoliasearch("19A6FWAAB3", 'a480e1583c97f14a6ad92c7c605d9f23');
+    var index = client.initIndex('developer-dot');
+    var facets = [];
+
+    if (productfacet) facets.push("product:" + productfacet);
+    if (doctypefacet) facets.push("doctype:" + doctypefacet);
+
+    index.search(queryparam, {
+        attributesToRetrieve: ['title', 'url'],
+        hitsPerPage: 50,
+        facetFilters: facets,
+    }, function searchDone(err, content) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        var results = "";
+        if (content.hits.length === 0) {
+            results += "<li>No Results Found</li>"
+        }
+        for (var h in content.hits) {
+            results += "<li><a href='" + content.hits[h].url + "'>" + content.hits[h].title + "</a></li>";
+        }
+        document.getElementById("search-results").innerHTML = results;
+    });
+
+    $('.search-button').click(function(e) {
+        var newurl = '/search/?q=' + encodeURIComponent($("#query").val());
+
+        if ($("#product-facet").val()) {
+            newurl += "&product=" + encodeURIComponent($("#product-facet").val());
+        }
+        if ($("#doctype-facet").val()) {
+            newurl += "&doctype=" + encodeURIComponent($("#doctype-facet").val());
+        }
+
         location.href = newurl;
     });
 });
