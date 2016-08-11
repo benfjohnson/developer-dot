12f197a7f216384ebcd73aa598a4d22eaba9f489
Ben Johnson <ben.johnson@avalara.com>
2016-08-11 12:34:33 -0700

    Fixed border issue for documentation sections

--- dynamic/react/api-app/components/apiDocumentationHeader.jsx
+++ dynamic/react/api-app/components/apiDocumentationHeader.jsx
@@ -29,14 +29,11 @@ const ApiDocumentationHeader = ({documentationFor, endpointId, isArray, nestingL
             <div className={`row api-documentation-section-header ${getSectionHighlightFromNestingLevel(nestingLevel)}`} data-target={`#${endpointId}-${documentationFor}-${propertyName.replace(/:/g, '')}`} data-toggle={'collapse'} onClick={() => {
                 onToggleDocCollapse(documentationFor, propertyName, endpointId);
             }}>
-                <div className={'col-md-1 col-sm-2 col-xs-2 documentation-expand-icon' + (!uiState.visible ? '' : ' rotate')}>
+                <div className={'col-md-11 col-xs-10'}>
+                    <span className={'api-doc-parameter-name'}>{`${isArray ? 'Array[' : ''}${displayName.charAt(0).toUpperCase() + displayName.slice(1)}${isArray ? ']' : ''}`}</span>
+                </div>
+                <div className={'col-md-1 col-xs-2 documentation-expand-icon' + (!uiState.visible ? '' : ' rotate')}>
                         <ExpanderIcon />
-                </div>
-                <div className={'col-sm-5 col-xs-5'}>
-                    <span className={'api-doc-parameter-name s5 api-doc-left-col'}>{displayName}</span>
-                </div>
-                <div className={'col-sm-5 col-xs-5'} style={{textAlign: 'right'}}>
-                    <span style={{fontWeight: 'bold'}}>{`${isArray ? 'Array[' : ''}${displayName.charAt(0).toUpperCase() + displayName.slice(1)}${isArray ? ']' : ''}`}</span>
                 </div>
             </div>
             <div>
