878835e2f5eca30233df26aa46a0d1fd68912047
Josh Sherman <josh.sherman@avalara.com>
2016-09-02 12:26:13 -0700

    React redux testing: API App reducers

--- dynamic/swagger-to-api-doc.js
+++ dynamic/swagger-to-api-doc.js
@@ -2,7 +2,7 @@ import path from 'path';
 import React from 'react';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import {reducer} from './react/api-app/reducers/reducer';
+import reducer from './react/api-app/reducers/reducer';
 import SwaggerParser from 'swagger-parser';
 import App from './react/api-app/app';
 import {renderToString} from 'react-dom/server';
