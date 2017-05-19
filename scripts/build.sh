98c486fcd90d069a28f27c839843ac1c83da41ef
JeremyCraigMartinez <JeremyCraigMartinez@users.noreply.github.com>
2017-05-19 10:02:47 -0700

    DX-340: simplify npm package.json scripts (#243)
    * DX-340: simplify npm package.json scripts
    
    Moved build, dev and test-web into a series
    of bash scripts in scripts/ directory.
    
    Common function are found in scripts/helpers.sh.
    
    test-web script was complicated so I moved it
    out of package.json and into the bash script.
    Other tests were straight forward enough
    to leave as one npm script.
    
    Once we start running test-web and
    test-web-bstack in ci, it will be cleaner to
    move all tests into common scripts/test.sh
    bash script
    
    * DX-340: remove redundant pretest

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# import helper functions
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/helpers.sh
+
+init
+
+bash -c "$build_css"
+browserify_bundle "dynamic/react/api-app/index.js" "public/js/api-bundle.js"
+browserify_bundle "dynamic/react/get-started/index.js" "public/js/get-started-bundle.js"
+browserify_bundle "dynamic/react/api-recipes/index.js" "public/js/recipe-bundle.js"
---
+++ scripts/dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# import helper functions
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/helpers.sh
+
+init
+
+watch "$build_css" _sass |
+    watchify_bundle "dynamic/react/api-app/index.js" "public/js/api-bundle.js" |
+    watchify_bundle "dynamic/react/get-started/index.js" "public/js/get-started-bundle.js" |
+    watchify_bundle "dynamic/react/api-recipes/index.js" "public/js/recipe-bundle.js" |
+    bundle exec jekyll serve
