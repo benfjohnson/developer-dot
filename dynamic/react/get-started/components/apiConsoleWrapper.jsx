11df029f6e67819aa30ab73d45a0839bc384b852
JeremyCraigMartinez <JeremyCraigMartinez@users.noreply.github.com>
2017-06-26 11:25:11 -0700

    remove number printed above get-started console (#328)

--- dynamic/react/get-started/components/apiConsoleWrapper.jsx
+++ dynamic/react/get-started/components/apiConsoleWrapper.jsx
@@ -3,13 +3,12 @@ import ApiConsole from '../../shared/components/apiConsole';
 
 /* eslint-disable react/prop-types */
 const ApiConsoleWrapper = (props) => {
+    setTimeout(function() {
+        $('.console-tool-tip').tooltip();
+    }, 1000);
+
     return (
         <div className={'tab-pane' + (props.endpoint.id === 0 ? ' active' : '')} id={props.endpoint.name.replace(/\s/g, '')} role='tabpanel' style={{margin: '10px'}}>
-            {
-                setTimeout(function() {
-                    $('.console-tool-tip').tooltip();
-                }, 1000)
-            }
             <ApiConsole {...props} />
         </div>
     );
